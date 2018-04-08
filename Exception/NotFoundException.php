3cec0d2967fcc48ed0b61d5bad23d372c3614523
Alexander Chibrikin <alek13.me@gmail.com>
2018-04-09 01:06:57 +0300

    extract Route::resolve(). closes #96.

---
+++ Exception/NotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Colibri\Routing\Exception;
+
+use LogicException;
+
+/**
+ * NotFoundException - if not found Module or Controller or Method in it.
+ */
+class NotFoundException extends LogicException
+{
+}
